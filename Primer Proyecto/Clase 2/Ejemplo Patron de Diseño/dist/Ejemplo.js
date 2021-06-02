4992b82635ef413216cd57acc9606d2c04fea0d9
Haroldo Arias <ihpam25@gmail.com>
2021-06-01 23:56:42 -0600

    se agrega gitignore

--- Primer Proyecto/Clase 2/Ejemplo Patron de Diseño/dist/Ejemplo.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Resta_1 = require("./Resta");
-const Suma_1 = require("./Suma");
-const suma = new Suma_1.Suma();
-console.log(suma.obtenerResultado(1, 2));
-const resta = new Resta_1.Resta();
-console.log(resta.obtenerResultado(1, 2));
--- Primer Proyecto/Clase 2/Ejemplo Patron de Diseño/dist/Resta.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Resta = void 0;
-class Resta {
-    obtenerResultado(valor1, valor2) {
-        return valor1 + valor2;
-    }
-}
-exports.Resta = Resta;
